9d6e222fff564eee9b7c9ef50ecb3edfaa1c7982
DarkRacer <schtscherbakov@gmail.com>
2020-10-21 01:56:52 +0300

    add Getter and Setter for contract

--- src/main/java/com/nc/labs/entity/Contract.java
+++ src/main/java/com/nc/labs/entity/Contract.java
@@ -1,9 +1,14 @@
 package com.nc.labs.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Calendar;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public abstract class Contract {
     private int id;
     private Calendar startDate;
